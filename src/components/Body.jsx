e45fdc11856d7940d9c93989322316ac71d00219
carbojet <carbojet@gmail.com>
2026-02-02 11:26:24 +0530

    episode 6 understanding api fetch react hooks useStage and click and change events

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,37 +1,72 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RestaurantList from "./RestaurantList";
-import { RESTAURANTS_MOCK_DATA } from "../utils/constants"; 
 
 //body component
 const Body = () => {
+  useEffect(() => {
+    fetchRestaurantData();
+  }, []);
+
+  const fetchRestaurantData = async () => {
+    try {
+        fetch("https://www.namastedev.com/api/v1/listRestaurants")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          const fetchData = data?.data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+          setRestaurants(fetchData);
+          setFilteredRestaurants(fetchData);
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+        }); 
+    } catch (error) {
+        console.error("Fetch Error:", error);
+    }
+}
   //state variable for filtered restaurants
-  const [restaurants, setRestaurants] = useState(RESTAURANTS_MOCK_DATA);
+  const [restaurants, setRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
 
-  //filter handler for top rated restaurants
-  const FilterHandler = (props) => {
-    const { restaurant } = props;
-    const filteredList = restaurants.filter(
-      (restaurant) => restaurant.rating >= 4.5
-    );
-    setRestaurants(filteredList);
-    console.log("Filtered Restaurants:", filteredList);
-  };
 
-  //search bar
-  const SearchBar = (props) => {
-    const { restaurant } = props;
-    return (
-      <div className="search-bar">
-        <input type="text" placeholder="Search restaurants..." />
-        <button className="top-rated-filter" onClick={() => FilterHandler(restaurant)}>Top Rated</button>
-      </div>
-    );
-  };
+  //restaurant filter function
+  const getFilterdRestaurants = (filter) => {
 
+    const filteredList = restaurants.filter((restaurant) => {
+      if(filter.name){
+        return restaurant?.info?.name.toLowerCase().includes(filter.name.toLowerCase());
+      }
+      if(filter.avgRating){
+        console.log("Filtering for avgRating:", filter.avgRating);
+        return restaurant?.info?.avgRating >= filter.avgRating;
+      }
+      return true;
+    });
+    console.log("Filtered List:", filteredList);
+    setFilteredRestaurants(filteredList);
+  }
+
+  if(restaurants.length === 0){
+    return <h1>Loading...</h1>;
+  }
   return (
     <div className="body">
-      <SearchBar restaurant={restaurants} />
-      <RestaurantList restaurants={restaurants} />
+      {/* <SearchBar /> */}
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search restaurants..."
+          value={searchText}
+          onKeyUp={(e) => getFilterdRestaurants({name: e.target.value})}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button className="top-rated-filter" onClick={() => getFilterdRestaurants({avgRating: 4.5})}>
+          Top Rated
+        </button>
+      </div>
+      <RestaurantList restaurants={filteredRestaurants} />
     </div>
   );
 }
